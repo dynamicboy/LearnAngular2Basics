85a3682f559e9bfd5adec2e65a6ce20007680dde
陈荣刚 <dynamicman@aliyun.com>
2016-07-01 18:19:23 +0800

    test

---
+++ process/typescript/boot.ts
@@ -0,0 +1,13 @@
+import { Component } from 'angular2/core';
+import { bootstrap } from 'angular2/platform/browser'
+
+@Component({
+   selector: 'my-app',
+   templateUrl: 'partials/app.html'
+})
+
+class AppComponent{
+
+}
+
+bootstrap(AppComponent);
